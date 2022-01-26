ad26d9f77ebadb49f16c396c3e21bb2c1cf02195
Gino Tasis <gtasis84@gmail.com>
2022-01-26 13:28:28 -0500

    First init

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+require('dotenv').config()
+const path = require('path')
+const port = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
+app.listen(port, () => {
+    console.log("Listening on port " + port)
+})
